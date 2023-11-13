f151df49c230a0a5b3c4326305f8e20ece45dc3e
Amir Azizov <creator005@mail.ru>
2023-11-13 17:53:48 +0500

    TZ v0.2 image added

--- src/components/molecules/Avatar.tsx
+++ src/components/molecules/Avatar.tsx
@@ -14,7 +14,8 @@ export const Avatar: React.FC<IBlockProps> = (props) => {
                 height: "60px",
                 borderRadius: "50%",
                 backgroundColor: "#eee",
-                border: "1px solid #e1e1e1"
+                border: "1px solid #e1e1e1",
+                objectFit: 'cover'
             }} src={props.img}  />
             <h4 style={{ margin: 0, fontSize: '16px' }}>{props.name}</h4>
             <p>{props.rate}</p>
