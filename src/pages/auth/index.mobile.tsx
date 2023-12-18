6a580f78233768087e2c4cdbb853cd428f1b5d01
Amir Azizov <creator005@mail.ru>
2023-12-18 15:05:53 +0400

    hotfix

--- src/pages/auth/index.mobile.tsx
+++ src/pages/auth/index.mobile.tsx
@@ -1,41 +1,10 @@
-import {
-  IonBackButton,
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import { AuthPage } from '.';
-import { ArrowBackIosNewOutlined } from '@mui/icons-material';
 
 export function MobileAuthPage() {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar style={{ paddingTop: '15px' }}>
-          <IonBackButton
-            text={''}
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '30px',
-              height: '40px',
-            }}
-          >
-            <ArrowBackIosNewOutlined sx={{ color: '#fff' }} />
-          </IonBackButton>
-          <IonTitle
-            style={{
-              fontSize: '1.25rem',
-            }}
-          >
-            Sign in
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
+    <IonPage className="notabs">
+      <IonContent>
         <AuthPage />
       </IonContent>
     </IonPage>
