0734b31230d3c6f2bcba1915e7bb37748b4f2ffe
Amir Azizov <creator005@mail.ru>
2023-12-20 00:01:30 +0400

    hotfix: android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ru.sportytime',
   appName: 'frontend',
   webDir: 'dist',
   server: {
