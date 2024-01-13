15f361879bf8a1c2da8dcfaf250fa896b0311c36
Amir Azizov <creator005@mail.ru>
2024-01-13 17:17:49 +0400

    fix: android permissions

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,7 @@ import './index.css';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CapacitorUpdater } from '@capgo/capacitor-updater';
-import { Dialog } from '@capacitor/dialog';
 import './theme/variables.css';
-import { isPlatform } from '@ionic/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -24,19 +21,19 @@ root.render(
   </React.StrictMode>,
 );
 
-if (isPlatform('ios') || isPlatform('android')) {
-  CapacitorUpdater.addListener('updateAvailable', async (res) => {
-    try {
-      const { value } = await Dialog.confirm({
-        title: 'Доступно обновление!',
-        message: `Версия ${res.bundle.version} готова к загрузке. Начать загрузку сейчас?`,
-      });
+// if (isPlatform('ios') || isPlatform('android')) {
+//   CapacitorUpdater.addListener('updateAvailable', async (res) => {
+//     try {
+//       const { value } = await Dialog.confirm({
+//         title: 'Доступно обновление!',
+//         message: `Версия ${res.bundle.version} готова к загрузке. Начать загрузку сейчас?`,
+//       });
 
-      if (value) CapacitorUpdater.set(res.bundle);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+//       if (value) CapacitorUpdater.set(res.bundle);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
 
-  CapacitorUpdater.notifyAppReady();
-}
+//   CapacitorUpdater.notifyAppReady();
+// }
