c4853663d52b58a2acecabb5f047d958f69c4fb0
Amir Azizov <creator005@mail.ru>
2024-02-26 16:44:31 +0300

    show notifications on safari

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { Route, Redirect } from 'react-router-dom';
 import { setupIonicReact } from '@ionic/react';
-import React, { Suspense, useCallback, useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { mobileRoutes } from './routes';
 import { LoadingCircle } from './components/atoms/LoadingCircle';
 import { Capacitor } from '@capacitor/core';
 import { initializeApp } from 'firebase/app';
-import { firebaseConfig, vapidKey } from './notifications/firebase';
-import {
-  FirebaseMessaging,
-  GetTokenOptions,
-} from '@capacitor-firebase/messaging';
-
-import { toast, ToastContainer } from 'react-toastify';
+import { firebaseConfig } from './notifications/firebase';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Geolocation } from '@capacitor/geolocation';
 
@@ -26,67 +21,6 @@ const App: React.FC = () => {
 
   useEffect(() => {
     Geolocation.getCurrentPosition();
-  }, []);
-
-  const addListeners = useCallback(() => {
-    FirebaseMessaging.addListener('notificationReceived', (event) => {
-      toast(
-        <>
-          <p>{event.notification?.title}</p>
-          <p>{event.notification?.body}</p>
-        </>,
-      );
-    });
-
-    FirebaseMessaging.addListener('notificationActionPerformed', (event) => {
-      console.log('notificationActionPerformed: ', { event });
-    });
-  }, []);
-
-  useEffect(() => {
-    // for android 13+
-    // FirebaseMessaging.checkPermissions()
-
-    FirebaseMessaging.requestPermissions().then(({ receive }) => {
-      if (receive === 'granted') {
-        addListeners();
-
-        const options: GetTokenOptions = { vapidKey: vapidKey };
-
-        if (Capacitor.getPlatform() === 'web') {
-          if ('serviceWorker' in navigator) {
-            navigator.serviceWorker
-              .register('../firebase-messaging-sw.js')
-              .then(
-                (registration) =>
-                  (options.serviceWorkerRegistration = registration),
-              )
-              .catch(console.error);
-
-            navigator.serviceWorker.addEventListener(
-              'message',
-              (event: any) => {
-                const notification = new Notification(
-                  event.data.notification.title,
-                  {
-                    body: event.data.notification.body,
-                  },
-                );
-                notification.onclick = (event) => {
-                  console.log('notification clicked: ', { event });
-                };
-              },
-            );
-          }
-        }
-        FirebaseMessaging.getToken(options).then(({ token }) => {
-          console.log('token: ', token);
-        });
-      } else {
-        // console.log('safari test: ', receive);
-        FirebaseMessaging.requestPermissions();
-      }
-    });
   }, []);
 
   return (
